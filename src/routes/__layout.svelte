8c77b56c0f34c236a54026717b74ea6c31468dd3
Tony Rinaldi <tony@grazzy.com>
2022-07-09 17:54:18 -0500

    Add nProgress for route changes

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,17 +1,29 @@
 <script lang="ts">
+  import nProgress from 'nprogress';
+
+  import { navigating } from '$app/stores';
   import Navbar from '$lib/components/Navbar.svelte';
   import { user } from '$lib/sessionStore';
   import { supabase } from '$lib/supabase';
   import '../app.css';
+  import '../nprogress.css';
 
   user.set(supabase.auth.user());
 
   supabase.auth.onAuthStateChange((_, session) => {
     user.set(session?.user ?? null);
   });
+
+  $: {
+    const isSameRoute = $navigating?.from.pathname === $navigating?.to.pathname;
+
+    if ($navigating && !isSameRoute) {
+      nProgress.start();
+    } else {
+      nProgress.done();
+    }
+  }
 </script>
 
 <Navbar />
-<main>
-  <slot />
-</main>
+<slot />
