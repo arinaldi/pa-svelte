b489c586685b8e1260ec13b469b05401248c3e65
Tony Rinaldi <tony@grazzy.com>
2022-08-20 15:21:59 -0500

    Simplify passing session to Supabase

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,32 +1,13 @@
-import cookie from 'cookie';
-import type { Session, User } from '@supabase/supabase-js';
-
 import { supabase } from '$lib/supabase';
 import type { LayoutServerLoad } from './$types';
 
-interface Payload {
-  session: Session | null;
-  user: User | null;
-}
+export const load: LayoutServerLoad = async ({ locals }) => {
+  if (!locals.user) return {};
 
-export const load: LayoutServerLoad = async ({ request }) => {
-  const cookieHeader = request.headers.get('cookie');
-  const payload: Payload = {
-    session: null,
-    user: null,
+  return {
+    accessToken: locals.accessToken,
+    error: locals.error,
+    session: supabase.auth.session(),
+    user: locals.user,
   };
-
-  if (!cookieHeader) return payload;
-
-  const token = cookie.parse(cookieHeader)['sb-access-token'];
-
-  if (!token) return payload;
-
-  const { error, user } = await supabase.auth.api.getUser(token);
-
-  if (error) return payload;
-
-  payload.session = supabase.auth.session();
-  payload.user = user;
-  return payload;
 };
