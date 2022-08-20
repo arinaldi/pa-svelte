4edf674bcd724a3f89c8a0665a7a940c71104f89
Tony Rinaldi <tony@grazzy.com>
2022-08-20 08:54:42 -0500

    Add session to page data

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,27 +1,32 @@
 import cookie from 'cookie';
+import type { Session, User } from '@supabase/supabase-js';
+
 import { supabase } from '$lib/supabase';
-import type { LayoutServerLoad } from '../../.svelte-kit/types/src/routes/$types';
+import type { LayoutServerLoad } from './$types';
+
+interface Payload {
+  session: Session | null;
+  user: User | null;
+}
 
 export const load: LayoutServerLoad = async ({ request }) => {
   const cookieHeader = request.headers.get('cookie');
+  const payload: Payload = {
+    session: null,
+    user: null,
+  };
 
-  if (!cookieHeader) {
-    return { user: null };
-  }
+  if (!cookieHeader) return payload;
 
   const token = cookie.parse(cookieHeader)['sb-access-token'];
 
-  if (!token) {
-    return { user: null };
-  }
+  if (!token) return payload;
 
   const { error, user } = await supabase.auth.api.getUser(token);
-  const session = supabase.auth.session();
-  // console.log({ session });
 
-  if (error) {
-    return { user: null };
-  }
+  if (error) return payload;
 
-  return { session, user };
+  payload.session = supabase.auth.session();
+  payload.user = user;
+  return payload;
 };
